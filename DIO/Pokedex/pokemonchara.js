57a0ca73d92fcda99cc9c5dead210e4cd2f5761c
Kevin <kverdolincarvalho@gmail.com>
2023-08-31 10:25:59 -0300

    AdiÃcao de primeira versao da pokedex

---
+++ DIO/Pokedex/pokemonchara.js
@@ -0,0 +1,59 @@
+function convertPokemonHtml(pokemonhtml) {
+    console.log(pokemonhtml)
+    return `<div style="display: flex; justify-content: space-between;">
+                <div id="pokecore">
+                    <span id="name" style="font-weight: 500; text-transform: capitalize;">${pokemonhtml.name}</span>
+                    <div style="display:flex; padding: 5px 0px;">
+                        <span class="type">${pokemonhtml.types[0].type.name}</span>
+                        <span class="type">${pokemonhtml.types[1].type.name}</span>
+                    </div> 
+                    
+                </div>  
+                <span id="pokenumber" style="width: 50px; padding-top: 10px; font-size: 15px;">#${pokemonhtml.id}</span>
+            </div>
+            <img id="pokemonimg" src="${pokemonhtml.sprites.other.dream_world.front_default}" alt="${pokemonhtml.name}">
+            `
+
+}
+function convertStatusHtml(pokemonstatus) {
+        return `<table>
+                    <tr>
+                        <td>Base EXP</td>
+                        <td class="td1">${pokemonstatus.base_experience}</td>
+                    </tr>
+                    <tr>
+                        <td>Height</td>
+                        <td class="td1">${pokemonstatus.height}</td>
+                    </tr>
+                    <tr>
+                        <td>Weight</td>
+                        <td class="td1">${pokemonstatus.weight}</td>
+                    </tr>
+                    <tr>
+                        <td>Abilities</td>
+                        <td class="td1">${pokemonstatus.abilities[0].ability.name}, ${pokemonstatus.abilities[1].ability.name}</td>
+                    </tr>
+                </table>
+                `
+}
+function convertStatusHtml0(pokemonstatusgame) {
+    //console.log(pokemonstatusgame);
+    return `<table>
+                <caption>Games</caption>
+                <tr>
+                <td>Generation</td>
+                <td class="td1" style="text-transform: uppercase">${pokemonstatusgame.generation.name}</td>
+                    
+                </tr>
+                <tr>
+                    <td>First  Appearance</td>
+                    <td class="td1">${pokemonstatusgame.name}</td>
+                </tr>
+                <tr>
+                    <td>Region</td>
+                    <td class="td1">${pokemonstatusgame.regions[0].name}</td>
+                </tr>
+            </table>
+            `
+    
+}
